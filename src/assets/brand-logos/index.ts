73269fee27eb2a372f3ff395b0a70d71ec0fc26b
TusharSatam <tsatam91@gmail.com>
2024-02-21 22:32:29 +0530

    brand screen ui

--- src/assets/brand-logos/index.ts
+++ src/assets/brand-logos/index.ts
@@ -10,3 +10,25 @@ export {default as amazonLogoImage} from './amazon-logo.jpg';
 export {default as pepperfryLogoImage} from './pepperfry-logo.jpg';
 export {default as easemytripLogoImage} from './ease-my-trip-logo.jpg';
 export {default as makemytripLogoImage} from './makemy-trip-logo.jpg';
+
+export {default as Dhan} from './Dhan.jpeg';
+export {default as Fi} from './Fi.jpeg';
+export {default as Finshot} from './Finshot.jpeg';
+export {default as ditto} from './ditto.jpeg';
+export {default as cleartax} from './cleartax.jpeg';
+export {default as Beshak} from './Beshak.jpeg';
+export {default as ETMarket} from './ETMarket.jpeg';
+export {default as ETMoneyBrand} from './ETMoneyBrand.jpeg';
+export {default as Gpay} from './Gpay.jpeg';
+export {default as Jupiter} from './Jupiter.jpeg';
+export {default as moneycontrol} from './moneycontrol.jpeg';
+export {default as PhonePe} from './PhonePe.jpeg';
+export {default as paytm} from './paytm.jpeg';
+export {default as PolicyBazaar} from './PolicyBazaar.jpeg';
+export {default as Tax2Win} from './Tax2Win.jpeg';
+export {default as Upstox} from './Upstox.jpeg';
+export {default as Upstox1} from './Upstox1.jpeg';
+export {default as VakilSearch} from './VakilSearch.jpeg';
+export {default as ZeroDha} from './ZeroDha.jpeg';
+
+
